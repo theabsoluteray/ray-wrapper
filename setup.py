f139a8cf7f632f5abaa9bcc4273457de5d899eca
Ray.ly <120208761+theabsoluteray@users.noreply.github.com>
2024-12-31 15:28:21 +0530

    2nd commit

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ray_package',  # Your package name
+    name='ray_package', 
     version='0.1.0',
     packages=find_packages(),
-    install_requires=['requests'],  # Dependencies
-    description='A package to interact with Discord Guilds',
-    author='Your Name',
-    author_email='your_email@example.com',
-    url='https://github.com/yourusername/ray_package',  # Replace with your repo URL
+    install_requires=['requests'], 
+    description='A package to interact with Discord API',
+    author='harsh',
+    author_email='me.harshu.yt.04@gmail.com',
+    url='https://github.com/theabsolutera/ray-wrapper',  
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
