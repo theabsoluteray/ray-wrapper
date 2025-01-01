ba90d59f8d97c3edcd52f3e19b465526d2774339
Ray.ly <120208761+theabsoluteray@users.noreply.github.com>
2025-01-01 13:19:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ray_package', 
-    version='0.1.0',
+    name='rayuwu', 
+    version='0.2.0',
     packages=find_packages(),
     install_requires=['requests'], 
     description='A package to interact with Discord API',
     author='harsh',
     author_email='me.harshu.yt.04@gmail.com',
-    url='https://github.com/theabsolutera/ray-wrapper',  
+    url='https://github.com/theabsoluteray/ray-wrapper',  
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
